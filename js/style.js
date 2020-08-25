c2123275e7bf46302333612f99b02d6e7e5047e3
choiwjun <31870430+choiwjun@users.noreply.github.com>
2020-08-25 17:09:14 +0900

    Add files via upload

--- js/style.js
+++ js/style.js
@@ -20,7 +20,6 @@ $(document).ready(function(){
         $(this).addClass('on')
         $(this).children().show()
     })
-
     $("dt").css("background-image","url('images/main/img_dt_off.png')").css('background-repeat','no-repeat').css('background-position',' right 1rem top 50%');
     $("dt").click(function(){
         if($("+dd",this).css("display")=="none"){
@@ -36,22 +35,16 @@ $(document).ready(function(){
             $(this).css("background-image","url('images/main/img_dt_off.png')").css('background-repeat','no-repeat').css('background-position',' right 1rem top 50%');
         }
     });
-
     var floatPosition = parseInt($("#floatMenu").css('top'));
 	// 250px 이런식으로 가져오므로 여기서 숫자만 가져온다. parseInt( 값 );
-
 	$(window).scroll(function() {
 		// 현재 스크롤 위치를 가져온다.
 		var scrollTop = $(window).scrollTop();
 		var newPosition = scrollTop + floatPosition + "px";
-
 		/* 애니메이션 없이 바로 따라감 */
 		 $("#floatMenu").css('top', newPosition);
-		
-
 		$("#floatMenu").stop().animate({
 			"top" : newPosition
 		}, 500);
-
     }).scroll();
 })
